ad03f099ace4ee08a42d5706a7ce003b863ad14e
Sébastien Vermeille <sebastien.vermeille@gmail.com>
2017-04-03 17:01:56 +0200

    Javadoc update

---
+++ src/main/java/com/foo/listeners/SensorListener.java
@@ -0,0 +1,15 @@
+package com.foo.listeners;
+
+import org.aquapilot.aquabox.api.event.StateChangedEvent;
+import org.aquapilot.aquabox.api.listener.AquaboxListener;
+
+/**
+ * This class listener to any changes that happend on sensors
+ */
+public class SensorListener implements AquaboxListener {
+
+   @Override
+   public void onPinStateChanged(StateChangedEvent event) {
+
+   }
+}
